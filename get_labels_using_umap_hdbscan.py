4c82fe6fbb3dad49c651d4483af3e0b14f37b28f
Pruthvik-Reddy <pruthvikreddy.21@gmail.com>
2023-05-05 12:38:43 -0400

    Added UMAP reduction and HDBSCAN clustering

---
+++ get_labels_using_umap_hdbscan.py
@@ -0,0 +1,26 @@
+import numpy as np
+import umap
+import hdbscan
+import pandas as pd
+
+embeddings=np.load("all_sentence_embeddings.npy")
+print("The shape of embeddings before dmensionality reduction is : ",embeddings.shape )
+
+reduced_embeddings = umap.UMAP(n_neighbors=15, 
+                            n_components=10, 
+                            metric='cosine').fit_transform(embeddings)
+
+print("The shape of embeddings after dmensionality reduction is : ",reduced_embeddings.shape )
+
+
+cluster = hdbscan.HDBSCAN(min_cluster_size=15,
+                          metric='euclidean',                      
+                          cluster_selection_method='eom').fit(reduced_embeddings)
+
+labels=cluster.labels_
+df = pd.DataFrame(labels, columns=["labels"])
+
+df.to_csv("quora_project/cluster_labels.csv", index=False)
+print("The dimensions of cluster labels is : ",df.shape)
+
+
